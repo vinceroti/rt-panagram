d890fa85cb7f5b7efe88b52d677cd1885a74fc4c
Vince Roti <vince.roti@gmail.com>
2021-07-14 15:39:53 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  // required to lint *.vue files
+  plugins: ["prettier",],
+  // add your custom rules here
+  rules: {
+    "object-shorthand": "off",
+    "prettier/prettier": "error",
+    "camelcase": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+    "no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
